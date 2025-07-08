fb3400c4d3746fec91d011d47ce71ff238fd3614
MonkeyDluffySan <wwwwwahmedyounis@gmail.com>
2025-07-08 21:36:42 +0300

    added rateLimit

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -1,7 +1,11 @@
-
 import helmet from "helmet";
 import csrf from "csurf";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per window
+});
 
 // CSRF protection middleware
 const csrfProtection = csrf({ cookie: true });
@@ -10,4 +14,5 @@ export const applySecurity = (app) => {
   app.use(helmet());
   app.use(cookieParser());
   app.use(csrfProtection);
+  app.use(limiter);
 };
